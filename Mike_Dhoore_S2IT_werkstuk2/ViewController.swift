b7ed26a2ca71bc10d8217c9971c58846d1b3fe49
telefoon13 <mike.d.hoore@telenet.be>
2018-04-22 13:43:20 +0200

    Initial Commit

---
+++ Mike_Dhoore_S2IT_werkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mike_Dhoore_S2IT_werkstuk2
+//
+//  Created by student on 22/04/18.
+//  Copyright © 2018 Mike Dhoore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
