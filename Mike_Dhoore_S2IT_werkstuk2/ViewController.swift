67a125629655cf0d05c36031991b6df2250734b6
telefoon13 <mike.d.hoore@telenet.be>
2018-04-29 19:46:08 +0200

    Added string to 2DLocation and add annotations + delete them when refresh

--- Mike_Dhoore_S2IT_werkstuk2/ViewController.swift
+++ Mike_Dhoore_S2IT_werkstuk2/ViewController.swift
@@ -14,8 +14,9 @@
 import UIKit
 import CoreData
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     //make appDelegate
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -30,17 +31,43 @@ class ViewController: UIViewController {
         refresh()
         //print("op knop geduwd")
     }
+    //Location manager
+    let manager = CLLocationManager()
 
     override func viewDidLoad() {
         //SUPER
         super.viewDidLoad()
         
+        
+        //Set map and location
+        //Bron : https://www.youtube.com/watch?v=UyiuX8jULF4
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        //Get teh coredate from JSON
         getJSONtoCore()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Bron : https://www.youtube.com/watch?v=UyiuX8jULF4
+        let location = locations[0]
+        //Zoom niveau
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.1,0.1)
+        //Center of map
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        //The region on scree,n
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation,span)
+        //Add region to the map
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
     }
     
     //Refresh function
@@ -68,7 +95,7 @@ class ViewController: UIViewController {
         setLastDate()
         
         //Get new batch of data
-        let url = URL(string: "https://opendata.brussel.be/api/records/1.0/search/?dataset=opmerkelijke-bomen&rows=10")
+        let url = URL(string: "https://opendata.brussel.be/api/records/1.0/search/?dataset=opmerkelijke-bomen&rows=20")
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
             if error != nil
             {
@@ -135,9 +162,10 @@ class ViewController: UIViewController {
         do {
             opgehaaldeBomen = try self.managedContext.fetch(Boom.fetchRequest())
             var aantal = 0
+            var aantalAdres = 0
             for elkeBoom in opgehaaldeBomen
             {
-                print(elkeBoom.beplanting)
+                /*print(elkeBoom.beplanting)
                 print(elkeBoom.diameter_van_de_kroon)
                 print(elkeBoom.gemeente)
                 print(elkeBoom.hoogte)
@@ -147,11 +175,35 @@ class ViewController: UIViewController {
                 print(elkeBoom.soort)
                 print(elkeBoom.status)
                 print(elkeBoom.straat)
-                print("------------------------")
+                print("------------------------")*/
                 aantal += 1
+                
+                if elkeBoom.straat != nil {
+                    //Convert adress to location
+                    //Source :https://stackoverflow.com/questions/42279252/convert-address-to-coordinates-swift
+                    let geocoder = CLGeocoder()
+                    let address = elkeBoom.straat! + elkeBoom.gemeente!
+                    geocoder.geocodeAddressString(address, completionHandler: {(placemarks, error) -> Void in
+                        if((error) != nil){
+                            print("Error", error ?? "")
+                        }
+                        if let placemark = placemarks?.first {
+                            let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                            //Annotation
+                            let anno1 = MKPointAnnotation()
+                            //info van de Annotation
+                            anno1.coordinate = coordinates
+                            anno1.title = elkeBoom.soort
+                            //anno1.subtitle = "theSubtitle"
+                            //Add anno on map
+                            self.map.addAnnotation(anno1)
+                        }
+                        aantalAdres += 1
+                    })
+                }
+                
             }
-            //print(opgehaaldeBomen[2].straat!)
-            print ("Er zijn total " + String(aantal) + " bomen in de db")
+            print ("Er zijn total " + String(aantal) + " bomen in de db waarvan " + String(aantalAdres) + " met een adres endus een pin")
         } catch {
             fatalError("Failed to fetch : \(error)")
         }
@@ -173,6 +225,11 @@ class ViewController: UIViewController {
         } catch {
             // Error Handling
         }
+        
+        //Remove all annotations
+        //Source : https://stackoverflow.com/questions/32850094/how-do-i-remove-all-map-annotations-in-swift-2
+        let allAnnotations = self.map.annotations
+        self.map.removeAnnotations(allAnnotations)
         
     }
 
